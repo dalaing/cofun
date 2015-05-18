96f7a52f6b37ebde66a2d8316ff3a6e5bd41004f
Dave Laing <dave.laing.80@gmail.com>
2015-05-18 22:35:10 +1000

    Imports the cofun-pairing project

---
+++ code/cofun-pairing/src/Util/Pairing.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Util.Pairing (
+      Pairing(..)
+    , pairEffect
+    ) where
+
+import           Control.Comonad              (Comonad, extract)
+import           Control.Comonad.Trans.Cofree (CofreeT, unwrap)
+import           Control.Monad.Trans.Free     (FreeF (..), FreeT, runFreeT)
+import           Data.Functor.Identity        (Identity (..))
+
+class (Functor f, Functor g) => Pairing f g where
+    pair :: (a -> b -> r) -> f a -> g b -> r
+
+instance Pairing Identity Identity where
+   pair f (Identity a) (Identity b) = f a b
+
+instance Pairing ((->) a) ((,) a) where
+   pair p f = uncurry (p . f)
+
+instance Pairing ((,) a) ((->) a) where
+  pair p f g = p (snd f) (g (fst f))
+
+pairEffect :: (Pairing f g, Comonad w, Monad m)
+           => (a -> b -> r) -> CofreeT f w a -> FreeT g m b -> m r
+pairEffect p s c = do
+    mb <- runFreeT c
+    case mb of
+        Pure x -> return $ p (extract s) x
+        Free gs -> pair (pairEffect p) (unwrap s) gs
+
+{-
+class (Functor m, Functor f, Functor g) => PairingM f g m | f g -> m where
+    pairM :: (a -> b -> m r) -> f a -> g b -> m r
+
+pairEffectM :: (PairingM f g m, Comonad w, Monad m)
+            => (a -> b -> m r) -> CofreeT f w a -> FreeT g m b -> m r
+pairEffectM p s c = do
+    mb <- runFreeT c
+    case mb of
+        Pure x -> p (extract s) x
+        Free gs -> pairM (pairEffectM p) (unwrap s) gs
+-}
