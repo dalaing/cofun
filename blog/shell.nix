c658f81e231ded1301d01022bd50a053805c6695
Dave Laing <dave.laing.80@gmail.com>
2015-07-12 20:00:33 +1000

    Switches to Nix for the Hakyll blog.

---
+++ blog/shell.nix
@@ -0,0 +1,22 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, hakyll, stdenv }:
+      mkDerivation {
+        pname = "cofun-blog";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        buildDepends = [ base hakyll ];
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  drv = pkgs.haskell.packages.${compiler}.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
