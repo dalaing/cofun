7cc4c495f4b1501dbea8d1e55632d5eb67905745
Dave Laing <dave.laing.80@gmail.com>
2015-08-22 17:54:11 +1000

    Removes adder-coproduct-net

--- code/adder-coproduct-net/src/Components/Errors.hs
+++
@@ -1,9 +0,0 @@
-module Components.Errors (
-    NetError(..)
-  ) where
-
-data NetError = Disconnected
-              | UnexpectedRequest
-              | UnexpectedResponse
-              deriving (Eq, Show)
-
