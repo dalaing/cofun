c09e73945ec0d930ce590d81da8690ff9a8d5c9a
Dave Laing <dave.laing.80@gmail.com>
2015-08-22 17:52:31 +1000

    cofun-coproduct: Adds type level info for sums and products

--- code/cofun-coproduct/src/Util/Coproduct.hs
+++ code/cofun-coproduct/src/Util/Coproduct.hs
@@ -1,49 +1,73 @@
-{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeOperators         #-}
-{-# LANGUAGE UndecidableInstances     #-}
-module Util.Coproduct (
-      (:+:)(..)
-    , (:<:)(..)
-    , (:*:)(..)
-    , (*:*)
-    ) where
+{-# LANGUAGE UndecidableInstances #-}
+module Util.Coproduct where
 
-import           Util.Pairing        (Pairing (..))
+import Util.Pairing
 
-import           Control.Applicative (liftA2)
+import Control.Applicative
+import Data.Proxy
+import Data.Type.Equality
 
-data (f :+: g) x = InL (f x) | InR (g x)
+class NotIn (x :: (* -> *)) (l :: [(* -> *)])
+instance NotIn x '[]
+instance (NotIn x t, (x == h) ~ 'False) => NotIn x (h ': t)
 
-instance (Functor f, Functor g) => Functor (f :+: g) where
-  fmap f (InL x) = InL (fmap f x)
-  fmap f (InR x) = InR (fmap f x)
+data ProductF (l :: [(* -> *)]) (a :: *) where
+  POne  :: Functor h => h a -> ProductF (h ': '[]) a
+  PAdd  :: (Functor h, NotIn h t) => h a -> ProductF t a -> ProductF (h ': t) a
 
-class (Functor sub, Functor sup) => sub :<: sup where
-  inj :: sub a -> sup a
+instance Functor (ProductF l) where
+  fmap f (POne h) = POne (fmap f h)
+  fmap f (PAdd h t) = PAdd (fmap f h) (fmap f t)
 
-instance Functor f => f :<: f where
-    inj = id
+class PAppend (t :: [(* -> *)]) where
+    type I t :: * -> * 
+    (*:*) :: (Functor h, NotIn h t) => (a -> h a) -> (a -> (I t) a) -> a -> ProductF (h ': t) a
 
-instance (Functor f, Functor g) => f :<: (f :+: g) where
-    inj = InL
+instance Functor h => PAppend (h ': '[]) where
+    type I (h ': '[]) = h
+    (*:*) f g a = PAdd (f a) (POne (g a))
 
-instance (Functor f, Functor g, Functor h, f :<: g) => f :<: (h :+: g) where
-    inj = InR . inj
+instance PAppend (h ': (i ': j)) where
+    type I (h ': (i ': j)) = ProductF (h ': (i ': j))
+    (*:*) f g a = PAdd (f a) (g a)
 
-data (f :*: g) x = f x :*: g x
+data SumF (l :: [(* -> *)]) (a :: *) where
+  SNext  :: (Functor h, NotIn h t) => SumF t a -> SumF (h ': t) a
+  SAdd   :: (Functor h, NotIn h t) => h a -> SumF (h ': t) a
 
-instance (Functor f, Functor g) => Functor (f :*: g) where
-  fmap h (f :*: g) = fmap h f :*: fmap h g
+instance Functor (SumF l) where
+  fmap f (SNext t) = SNext (fmap f t)
+  fmap f (SAdd h) = SAdd (fmap f h)
 
-(*:*) :: (Functor f, Functor g) => (a -> f a) -> (a -> g a) -> a -> (f :*: g) a
-(*:*) = liftA2 (:*:)
+class Contains (x :: (* -> *)) (xs :: [(* -> *)]) where
+  inj :: Functor x => x a -> SumF xs a
 
-instance (Pairing f f', Pairing g g') => Pairing (f :+: g) (f' :*: g') where
-  pair p (InL x) (a :*: _) = pair p x a
-  pair p (InR x) (_ :*: b) = pair p x b
+instance (NotIn x t) => Contains x (x ': t) where
+  inj = SAdd
 
-instance (Pairing f f', Pairing g g') => Pairing (f :*: g) (f' :+: g') where
-  pair p (a :*: _) (InL x) = pair p a x
-  pair p (_ :*: b) (InR x) = pair p b x
+instance (Functor h, NotIn h t, Contains x t) => Contains x (h ': t) where
+  inj = SNext . inj
 
+instance (Pairing h1 h2) => Pairing (ProductF (h1 ': '[])) (SumF (h2 ': '[])) where
+  pair f (POne ph) (SAdd sh) = pair f ph sh
+
+instance (Pairing h1 h2, Pairing (ProductF (t1 ': u1)) (SumF (t2 ': u2))) => Pairing (ProductF (h1 ': (t1 ': u1))) (SumF (h2 ': (t2 ': u2))) where
+  pair f (PAdd ph _) (SAdd sh) = pair f ph sh
+  pair f (PAdd _ pt) (SNext st) = pair f pt st
+
+instance (PairingM h1 h2 m) => PairingM (ProductF (h1 ': '[])) (SumF (h2 ': '[])) m where
+  pairM f (POne ph) (SAdd sh) = pairM f ph sh
+
+instance (PairingM h1 h2 m, PairingM (ProductF (t1 ': u1)) (SumF (t2 ': u2)) m) => PairingM (ProductF (h1 ': (t1 ': u1))) (SumF (h2 ': (t2 ': u2))) m where
+  pairM f (PAdd ph _) (SAdd sh) = pairM f ph sh
+  pairM f (PAdd _ pt) (SNext st) = pairM f pt st
