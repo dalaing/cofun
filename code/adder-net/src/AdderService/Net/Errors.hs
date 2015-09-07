8d963e8a1fc6743cc9c232c86435417994ed042f
Dave Laing <dave.laing.80@gmail.com>
2015-09-07 23:17:50 +1000

    Removes adder-net

--- code/adder-net/src/AdderService/Net/Errors.hs
+++
@@ -1,9 +0,0 @@
-module AdderService.Net.Errors (
-    NetError(..)
-  ) where
-
-data NetError = Disconnected
-              | UnexpectedRequest
-              | UnexpectedResponse
-              deriving (Eq, Show)
-
